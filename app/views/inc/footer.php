48b0e0065476c6dcefe5fc57628fc8c040528ffb
mattbtindall <matthewbtindall@gmail.com>
2022-11-18 09:23:45 +0000

    Update links in public files to use correct paths

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,5 +1,5 @@
     </div>
-<script src="<?= URL_ROOT ?>/js/main.js"></script>
+<script src="<?= URL_ROOT_BASE ?>/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
