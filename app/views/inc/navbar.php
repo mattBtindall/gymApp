f137543810d4eaa3191550dbe043c28b00071e6d
mattbtindall <matthewbtindall@gmail.com>
2022-11-26 10:47:51 +0000

    Add search bar with minor styling

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,27 +1,54 @@
 <nav class="navbar navbar-expand-lg <?= (AREA === 'User') ? 'bg-light' : 'bg-secondary';  ?>">
-  <div class="container">
+	<div class="container">
 
-    <a class="navbar-brand" href="<?= URL_ROOT ?>"><?= SITE_NAME; ?></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+		<a class="navbar-brand" href="<?= URL_ROOT ?>"><?= SITE_NAME; ?></a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link" href="<?= URL_ROOT; ?>">Home</a>
-        <a class="nav-link" href="<?= URL_ROOT; ?>/pages/about">About</a>
-      </div>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-link" href="<?= URL_ROOT; ?>">Home</a>
+				<a class="nav-link" href="<?= URL_ROOT; ?>/pages/about">About</a>
+			</div>
 
-      <div class="navbar-nav ms-auto">
-        <?php if (isLoggedIn()) : ?>
+			<div class="navbar-nav ms-auto">
+				<?php if (isLoggedIn()) : ?>
+					<form class="d-flex" role="search">
+						<div class="input-group">
+							<input class="form-control" type="search" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2">
+							<span class="input-group-text" id="basic-addon2">
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+									<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+								</svg>
+							</span>
+						</div>
+    				</form>
+
+					<!-- Pop up for search bar -->
+					<div class="search-bar-popover">
+						<div class="container">
+							<header class="search-bar-popover__header">
+								<ul>
+									<li>All</li>
+									<li>Clients</li>
+									<li>Active Members</li>
+								</ul>
+							</header>
+							<div class="search-bar-popover__output">
+
+							</div>
+						</div>
+					</div>
+
 					<a href="<?= URL_ROOT; ?>/users/profile" class="nav-link text-dark font-weight-bold"><?= isLoggedIn() ? $_SESSION['user_name'] . ' - ' . AREA : ''; ?></a>
 					<a href="<?= URL_ROOT; ?>/users/logout" class="nav-link">Logout</a>
-        <?php else : ?>
+				<?php else : ?>
 					<small class="nav-link text-dark border-end"><?= AREA; ?></small>
 					<a class="nav-link" href="<?= URL_ROOT; ?>/users/login">Login</a>
 					<a class="nav-link" href="<?= URL_ROOT; ?>/users/register">Register</a>
 				<?php endif; ?>
-      </div>
-    </div>
-  </div>
+			</div>
+		</div>
+	</div>
 </nav>
--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,25 @@
 'use strict';
+window.onload = init();
 
-const fileInput = document.getElementsByName('file')[0];
-const uploadbtn = document.querySelector('.upload');
+function init() {
+    const fileInput = document.getElementsByName('file')[0];
+    const uploadbtn = document.querySelector('.upload');
+    // document.body.classList.add('overlay-active');
 
-if (fileInput) {
-    const imgTypes = ['jpg', 'jpeg', 'png'];
+    if (fileInput) {
+        const imgTypes = ['jpg', 'jpeg', 'png'];
 
-    fileInput.addEventListener('change', function() {
-        if (!fileInput.files) {
-            return;
-        }
-
-        for (const imgType of imgTypes) {
-            if (fileInput.files[0].type.includes(imgType)) {
-                uploadbtn.disabled = false;
+        fileInput.addEventListener('change', function() {
+            if (!fileInput.files) {
                 return;
             }
-        }
-    });
+
+            for (const imgType of imgTypes) {
+                if (fileInput.files[0].type.includes(imgType)) {
+                    uploadbtn.disabled = false;
+                    return;
+                }
+            }
+        });
+    }
 }
