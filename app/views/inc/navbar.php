29ed3d26326023c479565e6322b37609013acea4
mattbtindall <matthewbtindall@gmail.com>
2022-11-28 09:50:57 +0000

    Add javascript to show/hide search popover. Style popover

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -14,9 +14,9 @@
 
 			<div class="navbar-nav ms-auto">
 				<?php if (isLoggedIn()) : ?>
-					<form class="d-flex" role="search">
+					<form class="d-flex search-bar-form" role="search">
 						<div class="input-group">
-							<input class="form-control" type="search" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2">
+							<input class="form-control search-bar" type="search" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2">
 							<span class="input-group-text" id="basic-addon2">
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
 									<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,10 @@ window.onload = init();
 function init() {
     const fileInput = document.getElementsByName('file')[0];
     const uploadbtn = document.querySelector('.upload');
-    // document.body.classList.add('overlay-active');
+    const searchBar = document.querySelector('.search-bar');
+    const searchBarPopover = document.querySelector('.search-bar-popover');
+    const isDescendant = (child, parent) => parent.contains(child);
+    let searchBarPopoverOpen = false;
 
     if (fileInput) {
         const imgTypes = ['jpg', 'jpeg', 'png'];
@@ -20,6 +23,26 @@ function init() {
                     return;
                 }
             }
+        });
+    }
+
+    if (searchBar) {
+        document.body.addEventListener('click', function(e) {
+            if (!searchBarPopoverOpen) {
+                return;
+            }
+
+            if (e.target !== searchBar && e.target !== searchBarPopover && !isDescendant(e.target, searchBarPopover)) {
+                searchBarPopoverOpen = false;
+                searchBarPopover.classList.remove('active');
+                document.body.classList.remove('overlay-active');
+            }
+        });
+
+        searchBar.addEventListener('focus', function() {
+            searchBarPopoverOpen = true;
+            searchBarPopover.classList.add('active');
+            document.body.classList.add('overlay-active');
         });
     }
 }
