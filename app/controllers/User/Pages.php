6350fbd00c352ecd6e7972be3d6f1ddd570ba0ff
Matt <m.tindall@kingfisherdirect.co.uk>
2022-11-29 12:11:17 +0000

    Alter description on user page

--- app/controllers/User/Pages.php
+++ app/controllers/User/Pages.php
@@ -7,7 +7,7 @@ class Pages extends Controller {
     public function index() {
         $data = [
             'title' => 'Welcome User',
-            'description' => 'Welcome to the gymApp User area. The gymApp allows you to gain access to gyms that use the gymApp system. All you need to do is create an account and then, once the repsective gym has granted your account access, your account will be assigned a unqiue barcode which you can scan upon entry of the gym. Please see the list of popular gymApp gyms below!'
+            'description' => 'Welcome to the gymApp User area. The gymApp allows you to gain access to gyms that use the gymApp system. All you need to do is create an account and then, once the repsective gym has granted your account access, your account will be assigned a unqiue barcode which you can scan to gain entry to the gym. Please see the list of popular gymApp gyms below!'
         ];
 
         $this->view('/pages/index', $data);
