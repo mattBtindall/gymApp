7a0f32f892cfdd9b5e5857f8b4fec83c5e891c70
mattbtindall <matthewbtindall@gmail.com>
2022-12-12 07:33:07 +0000

    Change javascript so the modals are in one file again

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,7 +1,6 @@
     </div>
 <?php loadPageSpecificJavaScript() ?>
-<script src="<?= URL_ROOT_BASE ?>/js/userModal.js"></script>
-<script src="<?= URL_ROOT_BASE ?>/js/search.js"></script>
+<script src="<?= URL_ROOT_BASE ?>/js/modal.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
