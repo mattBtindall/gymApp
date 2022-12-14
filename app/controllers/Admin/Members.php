4a09e3088f75a6131343eaba102d9e321490eed1
Matt <m.tindall@kingfisherdirect.co.uk>
2022-12-14 09:13:19 +0000

    pass user data from backend to frontend

--- app/controllers/Admin/Members.php
+++ app/controllers/Admin/Members.php
@@ -7,9 +7,21 @@ class Members extends Controller {
     public function index() {
         $members = $this->membersModel->getMembers($_SESSION['user_id']);
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+            if ($this->membersModel->addMembership()) {
+                // flash message here
+            } else {
+                // flash message here
+            }
+            
+            $this->view('members/index', $members);
         } else {
             $this->view('members/index', $members);
         }
+    }
+
+    public function getMembersData() {
+        // this is called from an ajax call from the frontend
+        // echo json_encode($this->membersModal->getMembers($_SESSION['user_id']));
+        echo $this->membersModel->getMembers($_SESSION['user_id']) ? json_encode($this->membersModel->getMembers($_SESSION['user_id'])) : '{}';
     }
 }
