bf66397a8724586e39c5e5d79ba8b5b8a6339839
Matt <m.tindall@kingfisherdirect.co.uk>
2022-12-20 17:59:11 +0000

    Separate userModal setModal functionality so the method only sets the modal

--- public/js/UserModal.js
+++ public/js/UserModal.js
@@ -14,22 +14,27 @@ class UserModal extends Modal {
     }
 
     setEventListeners() {
-        document.querySelector('.membership-table').addEventListener('click', (e) => this.setModal(e.target, '.account-link'));
+        document.querySelector('.membership-table').addEventListener('click', (e) => this.openModalClick(e.target, '.account-link'));
         document.querySelector('.search-bar-modal__output').addEventListener('click', (e) => {
             if (e.target.classList.contains('account-link')) {
-                this.setModal(e.target, '.search-modal__row');
+                this.openModalClick(e.target, '.search-modal__row');
             }
         });
 
         document.querySelector('.exit-modal-container i').addEventListener('click', () => this.closeModal());
     }
 
-    setModal(element, parentSelector) {
+    openModalClick(element, parentSelector) {
         const user = this.getCurrentUser(element, parentSelector);
+        this.setModal(user);
+    }
+
+    setModal(user) {
         this.elements.name.textContent = user.name;
         this.elements.email.textContent = user.email;
         this.elements.phone_number.textContent = user.phone_number;
         this.elements.dob.textContent = user.dob;
+        this.elements.id.value = user.id;
         this.openModal();
     }
 }
