4652af46fb48887ef31426e616048eb57bac0ff4
mattbtindall <matthewbtindall@gmail.com>
2022-12-30 13:37:30 +0000

    Make members page mobile freindly

--- app/views/Admin/members/index.php
+++ app/views/Admin/members/index.php
@@ -7,19 +7,20 @@
         <table>
             <tr>
                 <th>Name</th>
-                <th>Email</th>
-                <th>Phone Number</th>
+                <th class="d-none d-lg-table-cell">Email</th>
+                <th class="d-none d-lg-table-cell">Phone Number</th>
                 <th>Membership</th>
                 <th>Expiry Date</th>
             </tr>
+            <?php var_dump($data['members']); ?>
             <?php foreach($data['members'] as $row): ?>
                 <tr class="border-bottom border-top account-link">
                     <td>
                         <div class="img-container"><img src="<?= $row['img_url']; ?>"></div>
                         <?= $row['name']; ?>
                     </td>
-                    <td><?= $row['email']; ?></td>
-                    <td><?= $row['phone_number']; ?></td>
+                    <td class="d-none d-lg-table-cell"><?= $row['email']; ?></td>
+                    <td class="d-none d-lg-table-cell"><?= $row['phone_number']; ?></td>
                     <td><?= $row['term']; ?> <?= $row['term'] != 'custom' ? 'months' : ''; ?> </td>
                     <td><?= $row['expiry_date']; ?></td>
                     <td class="id" style="display: none;"><?= $row['id']; ?></td>
