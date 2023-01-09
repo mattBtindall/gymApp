9e083e6c200997c2b909b9c4c468c037b5f8277f
mattBtindall <matthewbtindall@gmail.com>
2023-01-09 09:21:20 +0000

    Change profile include so different areas can have different content on profile page
    Previously the profile pages for both areas had to be the same
    Performed so that customs terms can be performed on admin

--- app/views/Admin/users/profile.php
+++ app/views/Admin/users/profile.php
@@ -1,1 +1,4 @@
-<?php require APP_ROOT . '/views/inc/profile.php'; ?>
+<?php require APP_ROOT . '/views/inc/header.php'; ?>
+    <?php require APP_ROOT . '/views/inc/profile_content.php'; ?>
+    <h3>Terms</h3>
+<?php require APP_ROOT . '/views/inc/footer.php'; ?>
--- app/views/User/users/profile.php
+++ app/views/User/users/profile.php
@@ -1,1 +1,3 @@
-<?php require APP_ROOT . '/views/inc/profile.php'; ?>
+<?php require APP_ROOT . '/views/inc/header.php'; ?>
+    <?php require APP_ROOT . '/views/inc/profile_content.php'; ?>
+<?php require APP_ROOT . '/views/inc/footer.php'; ?>
