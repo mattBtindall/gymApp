25ae21370923466fc9b5f73e4da5bc452f380ada
mattBtindall <matthewbtindall@gmail.com>
2023-01-16 12:59:18 +0000

    Prepared backend to handle edits for terms

--- app/controllers/Admin/Terms.php
+++ app/controllers/Admin/Terms.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 class Terms extends Controller {
     public function __construct() {
         $this->termsModel = $this->model('Term');
@@ -18,17 +18,45 @@ class Terms extends Controller {
         }
     }
 
-    public function edit($termId) {
+    public function edit() {
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+        // split $_POST['term'] into term (e.g. month or week) and term_multipler (number that preceeds the term)
+        $termAndMultiplier = explode(' ', $_POST['term']);
+        // here we don't have errors from term or term_multiplier as they come from the drop down
+        $termUpdate= [
+            'display_name' => trim($_POST['display_name']),
+            'term' => $termAndMultiplier[1],
+            'term_multiplier' => $termAndMultiplier[0],
+            'cost' => trim($_POST['cost']),
+            'id' => $_POST['term_id'],
+            'display_name_err' => '',
+            'cost_err' => ''
+        ];
 
+        if (empty($termUpdate['display_name'])) {
+            $termUpdate['display_name_err'] = 'Please enter a display name';
+        }
+
+        if (empty($termUpdate['cost'])) {
+            $termUpdate['display_cost_err'] = 'Please enter a price';
+        }
+
+        $terms = $this->termsModel->getTerms($_SESSION['user_id']);
+        $data = [
+            'terms' => $terms,
+            'term_update' => $termUpdate
+        ];
+
+        $this->view('/terms/index', $data);
     }
 
     public function delete($termId) {
         if ($this->termsModel->deleteTerm($termId)) {
             flash('term_deleted', 'Term deleted successfully');
-            redirect('/terms/index');
         } else {
             flash('term_deleted', 'Term could not be deleted at this time please try again', 'alert alert-danger');
         }
+        redirect('/terms/index');
     }
 
 }
--- app/views/Admin/terms/index.php
+++ app/views/Admin/terms/index.php
@@ -9,6 +9,21 @@
     # Remind user that they can add a custom term in when adding a membership, these are just prest templates
     # list of terms:
     # one week, two week, one month, 6 week, two month, three month, six month, twelve month
+
+    // check to see if the term_update exists if it doesn't create it here
+    if (!isset($data['term_update'])) {
+        $data['term_update'] = [
+            'display_name' => '',
+            'term' => '',
+            'term_multiplier' => '',
+            'cost' => '',
+            'id' => '',
+            'display_name_err' => '',
+            'cost_err' => ''
+        ];
+    }
+
+    var_dump($data['term_update']);
 ?>
 <?php require APP_ROOT . '/views/inc/header.php'; ?>
 <?php
@@ -30,42 +45,45 @@
         <tr>
             <th>Display name</th>
             <th>Term</th>
-            <th>Cost</th>
+            <th>Cost (£)</th>
             <th>Date created</th>
         </tr>
         <?php foreach($data['terms'] as $term): ?>
             <?php
                 // format the expiry date to d/m/y
-                $expiryDate = date_create_from_format(SQL_DATE_TIME_FORMAT, $term['created_at']);
-                $expiryDate = $expiryDate->format(OUTPUT_DATE_TIME_FORMAT);
+                $dateCreated= date_create_from_format(SQL_DATE_TIME_FORMAT, $term['created_at']);
+                $dateCreated = $dateCreated->format(OUTPUT_DATE_TIME_FORMAT);
                 $combinedTermMultiplier = $term['term_multiplier'] . ' ' . $term['term'];
             ?>
             <tr>
-                <td><input type="text" value="<?= $term['display_name']; ?>" class="form-control form-control-lg w-50 terms-edit__display-name" disabled data-term-number="<?= $i; ?>"></td>
-                <td>
-                    <select class="form-control form-control-lg terms-edit__drop-down" name="term" data-term-number="<?= $i; ?>">
-                        <option value="please_select">Please select</option>
-                        <!-- user can't select a term that has already been sued but is not the current term as they're editing that -->
-                        <option value ="1 week" class="<?= ($combinedTermMultiplier !== '1 week' && in_array('1 week', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '1 week' ? 'selected' : ''; ?>>1 Week</option>
-                        <option value ="2 week" class="<?= ($combinedTermMultiplier !== '2 week' && in_array('2 week', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '2 week' ? 'selected' : ''; ?>>2 Weeks</option>
-                        <option value ="1 month" class="<?= ($combinedTermMultiplier !== '1 month' && in_array('1 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '1 month' ? 'selected' : ''; ?>>1 month</option>
-                        <option value ="2 month" class="<?= ($combinedTermMultiplier !== '2 month' && in_array('2 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '2 month' ? 'selected' : ''; ?>>2 Months</option>
-                        <option value ="3 month" class="<?= ($combinedTermMultiplier !== '3 month' && in_array('3 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '3 month' ? 'selected' : ''; ?>>3 Months</option>
-                        <option value ="6 month" class="<?= ($combinedTermMultiplier !== '3 month' && in_array('3 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '3 month' ? 'selected' : ''; ?>>6 Months</option>
-                        <option value ="9 month" class="<?= ($combinedTermMultiplier !== '9 month' && in_array('9 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '9 month' ? 'selected' : ''; ?>>9 Months</option>
-                        <option value ="12 month" class="<?= ($combinedTermMultiplier !== '12 month' && in_array('12 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '12 month' ? 'selected' : ''; ?>>12 Months</option>
-                        <option value ="24 month" class="<?= ($combinedTermMultiplier !== '24 month' && in_array('24 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '24 month' ? 'selected' : ''; ?>>24 Months</option>
-                        <option value ="36 month" class="<?= ($combinedTermMultiplier !== '36 month' && in_array('36 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '36 month' ? 'selected' : ''; ?>>36 Months</option>
-                    </select>
-                    <span class="terms-edit__term active" data-term-number="<?= $i; ?>"><?php echo $combinedTermMultiplier; echo $term['term_multiplier'] > 1 ? 's' : ''; ?></span>
-                </td>
-                <td><input type="text" value="£<?= $term['cost']; ?>" class="form-control form-control-lg w-50 terms-edit__cost" disabled data-term-number="<?= $i; ?>"></td>
-                <td><?= $expiryDate ;?></td>
-                <td><button href="<?= URL_ROOT; ?>/terms/edit" class="btn btn-success terms-edit__submit" data-term-number="<?= $i; ?>" disabled data-term-number="<?= $i; ?>">Submit</button></td>
-                <td><a href="" class="btn btn-warning terms-edit__edit" data-term-number="<?= $i; ?>">Edit</a></td>
-                <td><a href="<?= URL_ROOT; ?>/terms/delete/<?= $term['id']; ?>" class="btn btn-danger">Delete</a></td>
+                <form action="<?= URL_ROOT; ?>/terms/edit" method="POST">
+                    <td><input type="text" value="<?= $term['display_name']; ?>" class="form-control form-control-lg w-50 terms-edit__display-name" name="display_name" disabled data-term-number="<?= $i; ?>"></td>
+                    <td>
+                        <select class="form-control form-control-lg terms-edit__drop-down" name="term" data-term-number="<?= $i; ?>">
+                            <option value="please_select">Please select</option>
+                            <!-- user can't select a term that has already been sued but is not the current term as they're editing that -->
+                            <option value ="1 week" class="<?= ($combinedTermMultiplier !== '1 week' && in_array('1 week', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '1 week' ? 'selected' : ''; ?>>1 Week</option>
+                            <option value ="2 week" class="<?= ($combinedTermMultiplier !== '2 week' && in_array('2 week', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '2 week' ? 'selected' : ''; ?>>2 Weeks</option>
+                            <option value ="1 month" class="<?= ($combinedTermMultiplier !== '1 month' && in_array('1 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '1 month' ? 'selected' : ''; ?>>1 month</option>
+                            <option value ="2 month" class="<?= ($combinedTermMultiplier !== '2 month' && in_array('2 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '2 month' ? 'selected' : ''; ?>>2 Months</option>
+                            <option value ="3 month" class="<?= ($combinedTermMultiplier !== '3 month' && in_array('3 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '3 month' ? 'selected' : ''; ?>>3 Months</option>
+                            <option value ="6 month" class="<?= ($combinedTermMultiplier !== '3 month' && in_array('3 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '3 month' ? 'selected' : ''; ?>>6 Months</option>
+                            <option value ="9 month" class="<?= ($combinedTermMultiplier !== '9 month' && in_array('9 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '9 month' ? 'selected' : ''; ?>>9 Months</option>
+                            <option value ="12 month" class="<?= ($combinedTermMultiplier !== '12 month' && in_array('12 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '12 month' ? 'selected' : ''; ?>>12 Months</option>
+                            <option value ="24 month" class="<?= ($combinedTermMultiplier !== '24 month' && in_array('24 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '24 month' ? 'selected' : ''; ?>>24 Months</option>
+                            <option value ="36 month" class="<?= ($combinedTermMultiplier !== '36 month' && in_array('36 month', $combinedTermMultipliers)) ? 'd-none' : ''; ?>" <?= $combinedTermMultiplier == '36 month' ? 'selected' : ''; ?>>36 Months</option>
+                        </select>
+                        <span class="terms-edit__term active" data-term-number="<?= $i; ?>"><?php echo $combinedTermMultiplier; echo $term['term_multiplier'] > 1 ? 's' : ''; ?></span>
+                    </td>
+                    <td><input type="number" value="<?= $term['cost']; ?>" class="form-control form-control-lg w-50 terms-edit__cost" name="cost" disabled data-term-number="<?= $i; ?>"></td>
+                    <td><?= $dateCreated;?></td>
+                    <td><input class="btn btn-success terms-edit__submit" disabled data-term-number="<?= $i; ?>" type="submit" value="submit"></td>
+                    <td><a href="" class="btn btn-warning terms-edit__edit" data-term-number="<?= $i; ?>">Edit</a></td>
+                    <td><a href="<?= URL_ROOT; ?>/terms/delete/<?= $term['id']; ?>" class="btn btn-danger">Delete</a></td>
+                    <input type="hidden" name="term_id" value="<?= $term['id']; ?>">
+                </form>
             </tr>
-        <?php $i++ // incerment after so i starts from 0 as element arrays in js are zero indexed; ?> 
+        <?php $i++ // incerment after so i starts from 0 as element arrays in js are zero indexed; ?>
         <?php endforeach; ?>
     </table>
 <?php require APP_ROOT . '/views/inc/footer.php'; ?>
