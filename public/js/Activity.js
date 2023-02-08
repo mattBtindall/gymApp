e5a28abe0a8fb8998eb626a803afc5729ba3c5b0
mattBtindall <matthewbtindall@gmail.com>
2023-02-08 16:21:21 +0000

    WIP: Activity in js

---
+++ public/js/Activity.js
@@ -0,0 +1,5 @@
+// when the log button is pressed, or a barcode is scanned, if the user is on the 
+// 'activity' page render the new elements -> this can be done by comparing 
+// the last recieved activity elements to the new one. Only add new elements 
+// for the one that has been added
+// if not on the page just send request 
