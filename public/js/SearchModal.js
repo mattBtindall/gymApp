5ea55b44e203267e5af1e8a2efe1af69da344543
mattbtindall <matthewbtindall@gmail.com>
2023-02-24 14:11:32 +0000

    SearchModal: Style code

--- public/js/SearchModal.js
+++ public/js/SearchModal.js
@@ -3,9 +3,10 @@ import { userData, getData, getPhpMethodUrl, modals, getMembershipStatusClasses 
 
 export class SearchModal extends Modal {
     constructor() {
-        super({
-            output: '.search-bar-modal__output',
-        }, '.search-bar-modal');
+        super(
+            { output: '.search-bar-modal__output'},
+            '.search-bar-modal'
+        );
 
         this.elements.searchBar = document.querySelector('.search-bar');
         this.setEventListeners();
@@ -14,19 +15,7 @@ export class SearchModal extends Modal {
     setEventListeners() {
         const searchOutput = document.querySelector('.search-bar-modal__output');
         this.elements.searchBar.addEventListener('focus', (e) => this.openModal());
-
-        this.elements.searchBar.addEventListener('keyup', e => {
-            if (!e.target.value.length) {
-                this.setEmptyModalMessage('Type a name in the search bar');
-                return;
-            }
-
-            const url = getPhpMethodUrl("/Users/searchDb/", e.target.value);
-            getData(url)
-                .then(data => {
-                    this.setModal(data)
-                });
-        });
+        this.elements.searchBar.addEventListener('keyup', e => this.keyUpEventListener(e));
 
         if (searchOutput) {
             searchOutput.addEventListener('click', (e) => {
@@ -39,6 +28,19 @@ export class SearchModal extends Modal {
                 }
             });
         }
+    }
+
+    keyUpEventListener(e) {
+        if (!e.target.value.length) {
+            this.setEmptyModalMessage('Type a name in the search bar');
+            return;
+        }
+
+        const url = getPhpMethodUrl("/Users/searchDb/", e.target.value);
+        getData(url)
+            .then(data => {
+                this.setModal(data)
+            });
     }
 
     setEmptyModalMessage(message) {
@@ -59,57 +61,55 @@ export class SearchModal extends Modal {
         super.closeModal();
     }
 
+    createRow(userData) {
+        const rowTemplate = document.getElementById('row');
+        const rowBody = document.importNode(rowTemplate.content, true);
+        rowBody.querySelector('.row-img').src = userData['img_url'];
+        rowBody.querySelector('.name').textContent = userData['name'];
+        rowBody.querySelector('.email').textContent = userData['email'];
+        rowBody.querySelector('.phone_number').textContent = userData['phone_number'];
+        rowBody.querySelector('.id').textContent = userData['id'];
+
+        const logBtn = rowBody.querySelector('.log-btn');
+        logBtn.addEventListener('click', () => {
+            this.logMember(userData['id'])
+                .then(activity => {
+                    if (activity !== '{}' && this.activity) this.activity.addActivityElement(activity);
+                });
+        })
+
+        // set membership section
+        let membershipStatus = '';
+        switch (userData.status) {
+            case 'active' :
+                membershipStatus = userData.term_display_name;
+                break;
+            case 'expired' :
+                membershipStatus = 'Expired'
+                logBtn.classList.add('disabled');
+                break;
+            case 'future' :
+                membershipStatus = 'Starts'
+                logBtn.classList.add('disabled');
+                break;
+            default :
+                membershipStatus = '';
+                logBtn.classList.add('disabled');
+        }
+        rowBody.querySelector('.term-display-name').textContent = membershipStatus;
+        rowBody.querySelector('.expiry-date').textContent = userData.status ? userData.expiry_date : 'No membership';
+        const htmlClasses = getMembershipStatusClasses(userData.status, ['text']);
+        rowBody.querySelector('.membership-details').classList.add(...htmlClasses);
+
+        this.elements.output.appendChild(rowBody);
+    }
+
     setModal(data) {
         if (data === '{}') {
             this.setEmptyModalMessage('No user found with this name');
             return;
         }
-
         this.elements.output.innerHTML = "";
-        const createRow = (rowData) => {
-            // user details
-            const rowTemplate = document.getElementById('row');
-            const rowBody = document.importNode(rowTemplate.content, true);
-            rowBody.querySelector('.row-img').src = rowData['img_url'];
-            rowBody.querySelector('.name').textContent = rowData['name'];
-            rowBody.querySelector('.email').textContent = rowData['email'];
-            rowBody.querySelector('.phone_number').textContent = rowData['phone_number'];
-            rowBody.querySelector('.id').textContent = rowData['id'];
-
-            const logBtn = rowBody.querySelector('.log-btn');
-            logBtn.addEventListener('click', () => {
-                this.logMember(rowData['id'])
-                    .then(activity => {
-                        if (activity !== '{}' && this.activity) this.activity.addActivityElement(activity);
-                    });
-            })
-
-            // set membership section
-            let membershipStatus = '';
-            switch (rowData.status) {
-                case 'active' :
-                    membershipStatus = rowData.term_display_name;
-                    break;
-                case 'expired' :
-                    membershipStatus = 'Expired'
-                    logBtn.classList.add('disabled');
-                    break;
-                case 'future' :
-                    membershipStatus = 'Starts'
-                    logBtn.classList.add('disabled');
-                    break;
-                default :
-                    membershipStatus = '';
-                    logBtn.classList.add('disabled');
-            }
-            rowBody.querySelector('.term-display-name').textContent = membershipStatus;
-            rowBody.querySelector('.expiry-date').textContent = rowData.status ? rowData.expiry_date : 'No membership';
-            const htmlClasses = getMembershipStatusClasses(rowData.status, ['text']);
-            rowBody.querySelector('.membership-details').classList.add(...htmlClasses);
-
-            this.elements.output.appendChild(rowBody);
-        }
-
-        data.forEach(rowData => createRow(rowData));
+        data.forEach(userData => this.createRow(userData));
     }
 }
