25ec5cee448900e9a2ef5aef17cfd2af3867b7bb
mattbtindall <matthewbtindall@gmail.com>
2023-02-24 13:23:11 +0000

    Members: Style members table
    Add correct status colours
    Add active column to clearly display when membership is active

--- app/views/Admin/members/index.php
+++ app/views/Admin/members/index.php
@@ -12,8 +12,27 @@
                 <th class="d-none d-lg-table-cell">Phone Number</th>
                 <th>Membership</th>
                 <th>Expiry Date</th>
+                <th>Active</th>
             </tr>
             <?php foreach($data['members'] as $row): ?>
+                <?php
+                    $membershipStatus = getMembershipStatus($row['start_date'], $row['expiry_date']);
+                    $statusHtmlClass;
+                    switch ($membershipStatus) {
+                        case 'active' :
+                            $statusHtmlClass = 'text-success';
+                            break;
+                        case 'future' :
+                            $statusHtmlClass = 'text-info';
+                            break;
+                        case 'expired' :
+                            $statusHtmlClass = 'text-danger';
+                            break;
+                        default :
+                            $statusHtmlClass = 'text-warning';
+                    }
+                    $membershipOutput = $membershipStatus === 'active' ?  $row['term_display_name'] : ucfirst($membershipStatus);
+                ?>
                 <tr class="border-bottom border-top account-link">
                     <td>
                         <div class="img-container"><img src="<?= $row['img_url']; ?>"></div>
@@ -21,9 +40,12 @@
                     </td>
                     <td class="d-none d-lg-table-cell"><?= $row['email']; ?></td>
                     <td class="d-none d-lg-table-cell"><?= $row['phone_number']; ?></td>
-                    <td><?= $row['term_display_name']; ?></td>
-                    <td><?= formatForOutput($row['expiry_date']); ?></td>
+                    <td class="<?= $statusHtmlClass; ?>"><?= $membershipOutput?></td>
+                    <td class="<?= $statusHtmlClass; ?>"><?= formatForOutput($row['expiry_date']); ?></td>
                     <td class="id" style="display: none;"><?= $row['user_id']; ?></td>
+                    <td class="fs-4 <?= $membershipStatus === 'active' ? 'text-success': 'text-danger'; ?>">
+                        <?= $membershipStatus === 'active' ? GRANTED_ACCESS_ICON : NO_ENTRY_ICON; ?>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </table>
