923f8c44a7372de96b17ebabd573c39df6b0bd4b
mattBtindall <matthewbtindall@gmail.com>
2023-03-02 09:12:33 +0000

    Dashboard: getNumberOfActiveMembers on the backened

--- app/controllers/Admin/Dashboards.php
+++ app/controllers/Admin/Dashboards.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Used mainly for ajax calls to get data
+ * this works differently to other models
+ * as all the data is returned at once instead
+ * of indvidual function calls
+ */
+
 class Dashboards extends Controller {
     private $dashboardModel;
 
@@ -9,5 +16,9 @@ class Dashboards extends Controller {
 
     public function index() {
         $this->view('/dashboard/index');
+    }
+
+    public function getNumberOfActiveMembers() {
+        return $this->dashboardModel->getNumberOfActiveMembers($_SESSION['user_id']);
     }
 }
--- app/models/Dashboard.php
+++ app/models/Dashboard.php
@@ -6,4 +6,18 @@ class Dashboard {
     public function __construct() {
         $this->db = new Database();
     }
+
+    public function getNumberOfActiveMembers($adminId) {
+        $this->db->query('SELECT * FROM memberships WHERE admin_id = :adminId AND revoked = 0');
+        $this->db->bind(':adminId', $adminId);
+        $memberships = $this->db->resultSet(PDO::FETCH_ASSOC);
+
+        $numberOfAactiveMembers = 0;
+        if ($memberships) {
+            foreach($memberships as $membership) {
+                if (getMembershipStatus($membership['start_date'], $membership['expiry_date']) === 'active') $numberOfAactiveMembers++;
+            }
+        }
+        return $numberOfAactiveMembers;
+    }
 }
