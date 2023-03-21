93db85cffd5fcfac0ae42472f94bf4f5d0deb934
mattBtindall <matthewbtindall@gmail.com>
2023-03-21 12:29:39 +0000

    Add webpack
    Configure npm
    Configure webpack to bundle Js

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,6 +1,6 @@
     </div>
 
-<script src="<?= URL_ROOT_BASE ?>/js/main.js" type="module"></script>
+<script src="<?= URL_ROOT_BASE ?>/dist/bundled.js" type="module"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './public/js/main.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'public/dist'),
+    },
+};
